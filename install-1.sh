156586891a845b8b40878c5772786450c8100704
wangms168 <wms_88@163.com>
2021-09-09 15:37:18 +0800

    install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -6,8 +6,11 @@
 # Get dotfiles installation directory
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+ln -sf "$DOTFILES_DIR/.conkyrc" ~
+ln -sf "$DOTFILES_DIR/.dwm" ~
+ln -sf "$DOTFILES_DIR/.dwm/dwm.desktop" /usr/share/xsessions
 ln -sf "$DOTFILES_DIR/.gitconfig" ~
-ln -sf "$DOTFILES_DIR/.gitignore_global" ~
+ln -sf "$DOTFILES_DIR/.gitignore" ~
+ln -sf "$DOTFILES_DIR/.xprofile" ~
+ln -sf "$DOTFILES_DIR/.Xresources" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
-ln -sf "$DOTFILES_DIR/.zsh_exports" ~
-ln -sf "$DOTFILES_DIR/.zsh_aliases" ~
