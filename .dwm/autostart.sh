f8ce37e3225e4a863303593f5efbb3b61e232b0b
wangms168 <wms_88@163.com>
2021-11-03 14:19:26 +0800

    zsh plugin fzf

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,17 +1,17 @@
 # lanuch polkit auth agent
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
-pamac-tray &
-
-dex -a &
-# xrandr --output Virtual1 --mode 1920x1080 &
-picom -CGb &
-~/.fehbg &
-# nitrogen --restore &
-
-# dwmblocks 2>&1 >/dev/null &
-slstatus 2>&1 >/dev/null &
-
-# conky 
-pkill conky 
-conky -b &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# 
+# pamac-tray &
+# 
+# dex -a &
+# # xrandr --output Virtual1 --mode 1920x1080 &
+# picom -CGb &
+# ~/.fehbg &
+# # nitrogen --restore &
+# 
+# # dwmblocks 2>&1 >/dev/null &
+# slstatus 2>&1 >/dev/null &
+# 
+# # conky 
+# pkill conky 
+# conky -b &
